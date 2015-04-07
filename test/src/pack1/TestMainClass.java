e688b317d4056e3714ca00c284b8de8a9a097624
sebpapillon <sebastien.papillon@gmail.com>
2015-04-07 23:07:31 +0200

    first test

---
+++ test/src/pack1/TestMainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pack1;
+
+/**
+ * @author sebastien
+ *
+ */
+public class TestMainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ninja");
+
+	}
+
+}
