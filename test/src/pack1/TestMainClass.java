dda966807cd5d2d9af493908f7f9c36eebb9dd54
sebpapillon <sebastien.papillon@gmail.com>
2015-04-08 10:04:05 +0200

    commit

--- test/src/pack1/TestMainClass.java
+++ test/src/pack1/TestMainClass.java
@@ -14,7 +14,7 @@ public class TestMainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ninja");
+		System.out.println("powa ninja");
 
 	}
 
